caa2a2a3b4bf9e06d1611c07e34bf48602036ee5
Rose <natdog4519@gmail.com>
2017-11-14 15:29:51 -0500

    new commit

--- ElevenRose.Data/IdentityModels.cs
+++ ElevenRose.Data/IdentityModels.cs
@@ -4,12 +4,12 @@ using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 
-namespace Eleven.Web.Models
+namespace ElevenRose.Data
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
-    public class ApplicationUser : IdentityUser
+    public class ElevenRoseUser : IdentityUser
     {
-        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ElevenRoseUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
             var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
@@ -18,16 +18,16 @@ namespace Eleven.Web.Models
         }
     }
 
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    public class ElevenRoseDbContext : IdentityDbContext<ElevenRoseUser>
     {
-        public ApplicationDbContext()
+        public ElevenRoseDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
         }
 
-        public static ApplicationDbContext Create()
+        public static ElevenRoseDbContext Create()
         {
-            return new ApplicationDbContext();
+            return new ElevenRoseDbContext();
         }
     }
 }
